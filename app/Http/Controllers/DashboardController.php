33541ce360f7e7045737da9d6f4e7a75472f447e
sapta <saptamabar@gmail.com>
2025-10-05 00:13:40 +0700

    ...

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,9 +9,9 @@ class DashboardController extends Controller
 
     public function index()
     {
-        // $komunitas= auth()->user()->communities()->with('messages.user','users')->get();
-        $ikutKelas = auth()->user()->detailPembelians()->with('kelas')->get();
-        $kelas = auth()->user()->load('kelas');
+        // // $komunitas= auth()->user()->communities()->with('messages.user','users')->get();
+        // $ikutKelas = auth()->user()->detailPembelians()->with('kelas')->get();
+        // $kelas = auth()->user()->load('kelas');
         return view('dashboard.index', compact('kelas','ikutKelas','komunitas'));
     }
 
